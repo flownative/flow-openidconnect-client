917fc18bdb53be74944e040d03de3aa16cfe1cdb
Robert Lemke <robert@flownative.com>
2020-08-03 18:30:14 +0200

    Don't use Flow's base test case

--- Tests/Unit/OpenIdConnectClientTest.php
+++ Tests/Unit/OpenIdConnectClientTest.php
@@ -12,17 +12,16 @@ namespace Flownative\OpenIdConnect\Client;
  */
 
 use GuzzleHttp\Client as HttpClient;
-use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
-use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Neos\Cache\Backend\TransientMemoryBackend;
 use Neos\Cache\Frontend\VariableFrontend;
-use Neos\Flow\Tests\UnitTestCase;
+use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\StreamInterface;
+use ReflectionException;
 
-class OpenIdConnectClientTest extends UnitTestCase
+class OpenIdConnectClientTest extends TestCase
 {
     /**
      * @var OpenIdConnectClient
@@ -155,5 +154,41 @@ class OpenIdConnectClientTest extends UnitTestCase
         $this->jwksCache->set($cacheEntryIdentifier, $expectedJwks);
 
         $this->assertSame($expectedJwks, $this->oidcClient->getJwks());
+    }
+
+
+    /**
+     * Injects $dependency into property $name of $target
+     *
+     * This is a convenience method for setting a protected or private property in
+     * a test subject for the purpose of injecting a dependency.
+     *
+     * @param object $target The instance which needs the dependency
+     * @param string $name Name of the property to be injected
+     * @param mixed $dependency The dependency to inject – usually an object but can also be any other type
+     * @return void
+     * @throws ReflectionException
+     */
+    protected function inject($target, $name, $dependency): void
+    {
+        if (!is_object($target)) {
+            throw new \InvalidArgumentException('Wrong type for argument $target, must be object.');
+        }
+
+        $objectReflection = new \ReflectionObject($target);
+        $methodNamePart = strtoupper($name[0]) . substr($name, 1);
+        if ($objectReflection->hasMethod('set' . $methodNamePart)) {
+            $methodName = 'set' . $methodNamePart;
+            $target->$methodName($dependency);
+        } elseif ($objectReflection->hasMethod('inject' . $methodNamePart)) {
+            $methodName = 'inject' . $methodNamePart;
+            $target->$methodName($dependency);
+        } elseif ($objectReflection->hasProperty($name)) {
+            $property = $objectReflection->getProperty($name);
+            $property->setAccessible(true);
+            $property->setValue($target, $dependency);
+        } else {
+            throw new \RuntimeException('Could not inject ' . $name . ' into object of type ' . get_class($target));
+        }
     }
 }
